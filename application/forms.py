edffe9474c3789025cee13c8098a1c9463d8e495
hammad1015 <45166466+hammad1015@users.noreply.github.com>
2020-07-12 18:11:14 +0500

    Update forms.py

--- application/forms.py
+++ application/forms.py
@@ -4,13 +4,15 @@ from wtforms.validators import DataRequired, Length
 
 #Login Form 
 class LoginForm(FlaskForm):
+	
     email    = StringField('Email', validators=[DataRequired(), Length(min=1, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=25)])
     submit   = SubmitField('Login')
 
 #Create Buyer Form
 class CreateBuyerForm(FlaskForm):
-	id 	 	 = IntegerField('Buyer ID', validators=[DataRequired()])
+	
+	id 	 = IntegerField('Buyer ID', validators=[DataRequired()])
 	name 	 = StringField('Name', validators=[DataRequired(), Length(min=1, max=75)])
 	cnic 	 = IntegerField('CNIC', validators=[DataRequired()])
 	comments = TextField('Comments', validators=[])
@@ -19,11 +21,12 @@ class CreateBuyerForm(FlaskForm):
 
 #Create Deal Form 
 class CreateDealForm(FlaskForm):
-	id 						= IntegerField('Deal ID', validators=[DataRequired()])
-	buyer_id 				= IntegerField('Buyer ID', validators=[DataRequired()])
-	plot_id 				= IntegerField('Plot ID', validators=[DataRequired()])
-	first_amount_recieved 	= IntegerField('First Paid Amount',  validators=[DataRequired()])
-	amount_per_installment 	= IntegerField('Amount per Installment', validators=[DataRequired()])
-	installment_frequency 	= StringField('Installments per Year',  validators=[DataRequired()])
-	comments 				= TextField('Comments', validators=[])
-	submit 					= SubmitField('Create Deal')
+	
+	id 			= IntegerField('Deal ID', validators= [DataRequired()])
+	buyer_id 		= IntegerField('Buyer ID', validators= [DataRequired()])
+	plot_id 		= IntegerField('Plot ID', validators= [DataRequired()])
+	first_amount_recieved 	= IntegerField('First Paid Amount', validators= [DataRequired()])
+	amount_per_installment 	= IntegerField('Amount per Installment', validators= [DataRequired()])
+	installment_frequency 	= StringField ('Installments per Year', validators= [DataRequired()])
+	comments 		= TextField   ('Comments', validators= [])
+	submit 			= SubmitField ('Create Deal')
