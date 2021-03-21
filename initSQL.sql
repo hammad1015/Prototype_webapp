c80a34399f7347ba6bed5d627df5ec9b02fb7587
Guyroscopic <arafey183@gmail.com>
2021-03-21 19:14:26 +0500

    test commit

---
+++ initSQL.sql
@@ -0,0 +1,11 @@
+INSERT INTO user VALUES (1, "faisal@baap.com", "Faisal Rasool", "pop");
+INSERT INTO user VALUES (2, "rafey@baap.com", "Abdul Rafey", "pop");
+
+INSERT INTO buyer VALUES (1, "Hadi Ali", 12345,  null);
+INSERT INTO buyer VALUES (2, "Haider Ali", 12346,  null);
+
+INSERT INTO plot VALUES (1, "first", 2000000, "7 marla",  "not sold", "nothing");
+INSERT INTO plot VALUES (2, "second", 100000, "2 marla",  "not sold", "nothing");
+INSERT INTO plot VALUES (3, "third", 300000, "5 marla",  "not sold", "nothing");
+INSERT INTO plot VALUES (4, "fourth", 500000, "5 marla",  "not sold", "nothing");
+INSERT INTO plot VALUES (5, "fifth", 2000000, "7 marla",  "not sold", "nothing");
