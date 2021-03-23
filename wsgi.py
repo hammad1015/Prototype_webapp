928d347315233d0e8a149c98c30307dc1d16c663
Abdul Rafey <45163447+Guyroscopic@users.noreply.github.com>
2021-03-23 23:17:27 +0500

    Updated wsgi.py to use port 443

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     
     app.run(
         host='0.0.0.0',
-        port=80,
+        port=443,
         ssl_context=('./application//static/ssl/cert.pem', './application//static/ssl/key.pem'),
         use_reloader=False
     )
