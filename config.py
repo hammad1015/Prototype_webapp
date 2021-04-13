f33f95ef0e27f335ba553fd65253f0ad925fdf9c
pixus99 <45166466+pixus99@users.noreply.github.com>
2021-04-14 00:41:45 +0500

    updating git ignore

--- config.py
+++ config.py
@@ -3,22 +3,26 @@ from os import environ
 class Config:
     
     FLASK_APP           = 'wsgi.py'
-    SECRET_KEY          = environ.get("FlaskPrototypeSecretKey")
+    # SECRET_KEY          = environ.get("FlaskPrototypeSecretKey")
+    SECRET_KEY          = 'sectetKey'
     STATIC_FOLDER       = 'static'
     TEMPLATES_FOLDER    = 'templates'
 
     
 class DevelopmentConfig(Config):
     
-    ENV                     = environ.get('FlaskPrototypeDevEnv')
+    # ENV                     = environ.get('FlaskPrototypeDevEnv')
+    ENV                     = 'Development'
     DEBUG                   = True
     TESTING                 = True
-    SQLALCHEMY_DATABASE_URI = environ.get('FlaskPrototypeDevDB')  #'mysql://root:root@localhost/build_guild' 
+    # SQLALCHEMY_DATABASE_URI = environ.get('FlaskPrototypeDevDB')  
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/build_guild' 
 
     
 class ProductionConfig(Config):
     
-    ENV                         = environ.get('FlaskPrototypeProdEnv')
+    # ENV                         = environ.get('FlaskPrototypeProdEnv')
+    ENV                         = 'Production'
     DEBUG                       = False
     TESTING                     = False
     #SQLALCHEMY_DATABASE_URI    = # production db uri string #
--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     
     app.run(
         host        = '0.0.0.0',
-        port        = 443,
+        port        = 9000,
         ssl_context = ('./application//static//ssl//cert.pem', './application//static//ssl/key.pem'),
         use_reloader= False
     )
